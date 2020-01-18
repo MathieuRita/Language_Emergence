aee6b667510c5d5045971df17898f0beb655514c
Mathieu <mathieu.rita@polytechnique.edu>
2020-01-18 16:08:47 +0100

    update

--- EGG/egg/zoo/channel/train.py
+++ EGG/egg/zoo/channel/train.py
@@ -91,10 +91,13 @@ def dump(game, n_features, device, gs_mode):
     powerlaw_probs = 1 / np.arange(1, n_features+1, dtype=np.float32)
     powerlaw_probs /= powerlaw_probs.sum()
 
-    M=np.expand_dims(messages[0].cpu().numpy(),axis=0)
+    m0=messages[0].cpu().numpy()
+    m0=np.concatenate((m0,-np.ones((30))),axis=0)
+    M=np.expand_dims(m0,axis=0)
 
     for i in range(1,len(messages)):
         m=messages[i].cpu().numpy()
+        m=np.concatenate((m,-np.ones((30))),axis=0)
         M=np.concatenate((M,m),axis=0)
 
 
