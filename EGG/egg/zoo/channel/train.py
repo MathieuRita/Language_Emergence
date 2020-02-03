0731d6798d285efd9776fa5109cdfed770196905
Mathieu <mathieu.rita@polytechnique.edu>
2020-02-03 14:39:24 +0100

    save_dir

--- EGG/egg/zoo/channel/train.py
+++ EGG/egg/zoo/channel/train.py
@@ -203,7 +203,8 @@ def main(params):
     #trainer.train(n_epochs=opts.n_epochs)
 
     #if opts.checkpoint_dir:
-    trainer.save_checkpoint(name=f'{opts.name}_vocab{opts.vocab_size}_rs{opts.random_seed}_lr{opts.lr}_shid{opts.sender_hidden}_rhid{opts.receiver_hidden}_sentr{opts.sender_entropy_coeff}_reg{opts.length_cost}_max_len{opts.max_len}')
+    #trainer.save_checkpoint(name=f'{opts.name}_vocab{opts.vocab_size}_rs{opts.random_seed}_lr{opts.lr}_shid{opts.sender_hidden}_rhid{opts.receiver_hidden}_sentr{opts.sender_entropy_coeff}_reg{opts.length_cost}_max_len{opts.max_len}')
+    trainer.save_checkpoint(name='weights')
 
     all_messages=dump(trainer.game, opts.n_features, device, False)
     core.close()
